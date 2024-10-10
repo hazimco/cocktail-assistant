960fdaeae7f1e7e7ec48dc4c84aba4c0a90815ec
hazimco <140416762+hazimco@users.noreply.github.com>
2024-10-11 01:53:17 +0200

    displaying thumbnail, name and instructions in ResultItem

--- src/components/result-item.ts
+++ src/components/result-item.ts
@@ -6,7 +6,19 @@ interface Props {
 }
 
 const ResultItem = ({ drink }: Props) => {
-  return html`<div>${drink.strDrink}</div>
+  const {
+    strDrinkThumb: thumbnail,
+    strDrink: name,
+    strInstructions: instructions,
+  } = drink;
+
+  return html`<div class="result-card">
+      <img src=${thumbnail || ""} />
+      <div>
+        <h2>${name}</h2>
+        <p>${instructions}</p>
+      </div>
+    </div>
     ${style}`;
 };
 
@@ -14,4 +26,19 @@ customElements.define("result-item", component(ResultItem));
 
 // ################### styling ##############################
 
-const style = html`<style></style>`;
+const style = html`<style>
+  .result-card {
+    display: flex;
+    border: 1px solid gray;
+    border-radius: 10px;
+    padding: 10px;
+    gap: 30px;
+  }
+  img {
+    height: 100px;
+  }
+  h2,
+  p {
+    margin: 0px;
+  }
+</style>`;
