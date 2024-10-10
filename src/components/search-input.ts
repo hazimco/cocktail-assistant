8c2332b2b1b5360562165a79eb713e22ca5b39cc
hazimco <140416762+hazimco@users.noreply.github.com>
2024-10-10 13:34:01 +0200

    creating search input field and fetching data from 'TheCocktailDB'

---
+++ src/components/search-input.ts
@@ -0,0 +1,53 @@
+import { html, component, useState } from "haunted";
+
+const SearchInput = () => {
+  const [text, setText] = useState("");
+
+  const handleSubmit = async (event: Event) => {
+    event.preventDefault();
+
+    const cocktails = await fetchCocktails(text);
+    console.log(cocktails);
+  };
+
+  const handleInput = (event: Event) => {
+    const input = event.target as HTMLInputElement;
+
+    setText(input.value);
+  };
+
+  return html` <form @submit=${handleSubmit} class="container">
+      <input placeholder="Search for a drink..." @input=${handleInput} />
+      <button>Search</button>
+    </form>
+    ${style}`;
+};
+
+customElements.define("search-input", component(SearchInput));
+
+// ################### styling ##############################
+
+const style = html` <style>
+  .container {
+    display: flex;
+  }
+  input {
+    padding: 6px;
+  }
+</style>`;
+
+// ################### fetch logic ##############################
+
+const BASE_URL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
+
+const fetchCocktails = async (searchText: string) => {
+  try {
+    const response = await fetch(`${BASE_URL}${searchText}`);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+    return response.json();
+  } catch (error) {
+    console.log((error as Error).message);
+  }
+};
--- src/my-app.ts
+++ src/my-app.ts
@@ -1,7 +1,10 @@
 import { html, component } from "haunted";
+import "./components/search-input";
 
 const App = () => {
-  return html` <h1>Hello World</h1> `;
+  return html` <div>
+    <search-input></search-input>
+  </div>`;
 };
 
 customElements.define("my-app", component(App));
