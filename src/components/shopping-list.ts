0f5e9640831b8b710766b106ce6cce55bedfa75e
hazimco <140416762+hazimco@users.noreply.github.com>
2024-10-11 12:30:04 +0200

    showing print button only if there are ingredients in the list, and opening the browser print dialog when clicking the button

--- src/components/shopping-list.ts
+++ src/components/shopping-list.ts
@@ -5,12 +5,18 @@ interface Props {
 }
 
 const ShoppingList = ({ ingredients }: Props) => {
+  const handlePrint = () => {
+    print(); //hmm.. this includes the whole page, not just the shopping list
+  };
+
   return html`<div class="container">
       <h2>Shopping List</h2>
       <ul>
         ${ingredients.map((ingredient) => html`<li>${ingredient}</li>`)}
       </ul>
-      <button>Print</button>
+      ${ingredients.length > 0
+        ? html`<button @click=${handlePrint}>Print</button>`
+        : ""}
     </div>
     ${style}`;
 };
